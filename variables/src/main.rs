6a9607a91f55e76bd1dbcbe71753529588a25e7d
Aayush Mittal <aayushmittal1996@gmail.com>
2024-07-17 09:48:32 +0100

    Chapter 3 done

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
+    let x = x + 1;
     println!("The value of x is {x}");
-    x = 6;
+    {
+        let x = x + 1;
+        println!("The value of x is {x}");
+    }
     println!("The value of x is {x}");
 }
